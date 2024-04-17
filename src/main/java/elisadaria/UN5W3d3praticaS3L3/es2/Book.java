826fa84ea0a0fa09c5677471bc0fc35fa33975a2
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-17 16:29:36 +0200

    Create Book.java

---
+++ src/main/java/elisadaria/UN5W3d3praticaS3L3/es2/Book.java
@@ -0,0 +1,41 @@
+package elisadaria.UN5W3d3praticaS3L3.es2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book implements BookElement{
+    private List<BookElement> bookElementList=new ArrayList<>();
+    private List<String> authors=new ArrayList<>();
+    private double price;
+
+    public Book(List<String> authors, double price) {
+        this.authors = authors;
+        this.price = price;
+    }
+    public void addBookE(BookElement be){
+        bookElementList.add(be);
+    }
+    public void removeBookE(BookElement be){
+        bookElementList.remove(be);
+    }
+
+    @Override
+    public int getNofPages() {
+       int nOfPages=0;
+       for(BookElement element:bookElementList){
+           nOfPages+=element.getNofPages();
+       }
+       return nOfPages;
+    }
+
+    @Override
+    public void print() {
+        for(BookElement element:bookElementList){
+            element.print();
+        }
+    }
+    public void printEntireBook(){
+        System.out.println("stampa dell'intero libro");
+        print();
+    }
+}
