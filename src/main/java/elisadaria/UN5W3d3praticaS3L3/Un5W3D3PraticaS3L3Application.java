6d92ed9e7d23617d5c33d7628c52bb2a5febf6d4
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-17 15:11:49 +0200

    initializing

---
+++ src/main/java/elisadaria/UN5W3d3praticaS3L3/Un5W3D3PraticaS3L3Application.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W3d3praticaS3L3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Un5W3D3PraticaS3L3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Un5W3D3PraticaS3L3Application.class, args);
+	}
+
+}
---
+++ src/test/java/elisadaria/UN5W3d3praticaS3L3/Un5W3D3PraticaS3L3ApplicationTests.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W3d3praticaS3L3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Un5W3D3PraticaS3L3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
